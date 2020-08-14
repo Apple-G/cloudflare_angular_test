b91358680e7e301b14d6285a93f9b38eb6fb3e77
codezup <codezup@gmail.com>
2020-08-14 23:09:23 +0530

    Angular 10|9|8|7|6 checkboxes and multicheckboxes example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-checkboxes-example';
+  form: FormGroup;
+  CountryData: Array<any> = [
+    { name: 'IND', value: 'India' },
+    { name: 'AUS', value: 'Australia' },
+    { name: 'USA', value: 'America' },
+    { name: 'RUS', value: 'Rusia' },
+    { name: 'Eng', value: 'England' }
+  ];
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      checkArray: this.fb.array([])
+    })
+  }
+  onCheckboxChange(e) {
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+  
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: FormControl) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+  submitForm() {
+    console.log(this.form.value)
+  }
+  
+
 }
