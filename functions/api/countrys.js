46ea6aa61fc30953845c0215326926586b9680b3
Apple-G <tobiasgiesel@gmail.com>
2022-08-11 14:27:10 +0200

    Create countrys.js

---
+++ functions/api/countrys.js
@@ -0,0 +1,11 @@
+
+export const onRequest = () => {
+  CountryData: Array<any> = [
+    { name: 'IND', value: 'India' },
+    { name: 'AUS', value: 'Australia' },
+    { name: 'USA', value: 'America' },
+    { name: 'RUS', value: 'Rusia' },
+    { name: 'Eng', value: 'England' }
+  ];
+  return new Response(CountryData)
+}
