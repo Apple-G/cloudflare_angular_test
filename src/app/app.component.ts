776a636d899a0338ebd9435e3cb8f7b9605eb89e
Apple-G <tobiasgiesel@gmail.com>
2022-08-11 14:23:20 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
+import { ApiService } from './services/api.service.ts';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-checkboxes-example';
   form: FormGroup;
-  CountryData: Array<any> = [
+ /* CountryData: Array<any> = [
     { name: 'IND', value: 'India' },
     { name: 'AUS', value: 'Australia' },
     { name: 'USA', value: 'America' },
     { name: 'RUS', value: 'Rusia' },
     { name: 'Eng', value: 'England' }
   ];
-  constructor(private fb: FormBuilder) {
+  */
+  CountryData:any;
+  
+  constructor(private fb: FormBuilder, private apiService:ApiService) {
     this.form = this.fb.group({
       checkArray: this.fb.array([])
     })
   }
+  
+   ngOnInit() {
+      this.apiService.getPosts()
+        .subscribe(response => {
+          this.CountryData = response;
+        });
+  }
+  
   onCheckboxChange(e) {
     const checkArray: FormArray = this.form.get('checkArray') as FormArray;
   
